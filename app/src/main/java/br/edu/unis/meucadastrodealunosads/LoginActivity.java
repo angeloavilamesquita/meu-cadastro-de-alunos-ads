ab396dd7f510517b6dde14ef681bae456360f2fb
Angelo Avila Mesquita <angelo.mesquita@professor.unis.edu.br>
2023-10-31 20:19:43 -0300

    WIP
    -cria o layout do login com as validações e links para criar a conta
    -cria as activities de dashboard e new_account
    
    TODO: elaborar a regra de autenticação com o SQLite

---
+++ app/src/main/java/br/edu/unis/meucadastrodealunosads/LoginActivity.java
@@ -0,0 +1,87 @@
+package br.edu.unis.meucadastrodealunosads;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView newAccount;
+    EditText edtUser;
+    EditText edtPassword;
+    Button btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initiateWidgets();
+        makeCreateNewAccountButton();
+        AuthenticateUser();
+    }
+
+    private void initiateWidgets() {
+        edtUser = findViewById(R.id.login_edt_user);
+        edtPassword = findViewById(R.id.login_edt_password);
+        btnLogin = findViewById(R.id.login_btn_login);
+        newAccount = findViewById(R.id.login_txt_create_account);
+    }
+
+    private void makeCreateNewAccountButton() {
+        SpannableString ss = new SpannableString(newAccount.getText());
+        ClickableSpan cs = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View view) {
+                Intent intent = new Intent(LoginActivity.this, NewAccountActivity.class);
+                startActivity(intent);
+            }
+        };
+        ss.setSpan(cs, 0, newAccount.getText().length(), 0);
+        newAccount.setText(ss);
+        newAccount.setMovementMethod(LinkMovementMethod.getInstance());
+    }
+
+    private void AuthenticateUser() {
+        btnLogin.setOnClickListener(view -> {
+            String user = edtUser.getText().toString();
+            String password = edtPassword.getText().toString();
+
+            if (!validateCredentials(user, password)) {
+                Toast.makeText(
+                        LoginActivity.this, R.string.login_txt_fields_can_not_empty,
+                        Toast.LENGTH_SHORT
+                ).show();
+                return;
+            }
+
+            boolean isAuthenticated = authenticate(user, password);
+            if (!isAuthenticated) {
+                Toast.makeText(
+                        LoginActivity.this, R.string.login_txt_user_or_password_incorrets,
+                        Toast.LENGTH_SHORT
+                ).show();
+                return;
+            }
+            startActivity(new Intent(LoginActivity.this, DashboardActivity.class));
+        });
+    }
+
+    private boolean validateCredentials(String user, String password) {
+        return !user.isEmpty() && !password.isEmpty();
+    }
+
+    private boolean authenticate(String user, String password) {
+        // TODO implements authentication rule using SQLite
+        return user.equals("user") && password.equals("!@#123qwe");
+    }
+}
---
+++ app/src/main/java/br/edu/unis/meucadastrodealunosads/NewAccountActivity.java
@@ -0,0 +1,14 @@
+package br.edu.unis.meucadastrodealunosads;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_account);
+    }
+}
