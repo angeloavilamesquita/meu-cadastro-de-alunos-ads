f73ad910210a667c7e85902851a8f15bde9cbdd8
Angelo Avila Mesquita <angelo.mesquita@professor.unis.edu.br>
2023-11-07 21:23:04 -0300

    WIP

--- app/src/main/java/br/edu/unis/meucadastrodealunosads/NewAccountActivity.java
+++ app/src/main/java/br/edu/unis/meucadastrodealunosads/NewAccountActivity.java
@@ -1,14 +1,23 @@
 package br.edu.unis.meucadastrodealunosads;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class NewAccountActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        toolbar = findViewById(R.id.new_account_toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
